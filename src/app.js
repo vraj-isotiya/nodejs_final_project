f134905c6979f80cc841c512f81eb9f922cfdc33
vraj-isotiya <vraj.isotiya@triyanshitechnologies.com>
2025-12-29 16:27:22 +0530

    auto deploy EC2

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json({ limit: "10kb" }));
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded());
 
 app.use("/api", routes);
